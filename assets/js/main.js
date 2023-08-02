88883a78511445d8d0b4c74cede7374ba729fb3a
jojo <jasonlopezcc@gmail.com>
2023-08-02 16:04:55 -0600

    config webpack

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    let navbar = document.querySelector('#navbar');
-    let bonny = document.querySelector('#bonny');
-    bonny.addEventListener('click', () => {
-        navbar.classList.toggle('open');
-    }); 
-})()
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "../src/assets/js/main.js";
+import "../src/assets/js/fixed.js";
+import '../src/assets/css/index.css';
+import '../node_modules/eins-modal/dist/js/eins-modal.min.js';
